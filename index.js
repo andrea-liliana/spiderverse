d6de6cfea30bd6a16f5c7b07187601a347addf36
andrea-liliana <andrea.mx1998@gmail.com>
2022-04-30 03:48:36 +0200

    Archivo principal

---
+++ index.js
@@ -0,0 +1,7 @@
+const Spiderman = require('./app/spiderman');
+
+const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+console.log(andrewGarfield.name)
+console.log(andrewGarfield.age)
+console.log(andrewGarfield.getInfo())
